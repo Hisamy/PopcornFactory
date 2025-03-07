e9db0a0b459f7d766c01b4f9079d8cb6bcd88db2
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-03-06 23:52:19 -0700

    terminado

--- Practica6/app/src/main/java/mx/edu/itson/practica6/Catalogo.kt
+++ Practica6/app/src/main/java/mx/edu/itson/practica6/Catalogo.kt
@@ -37,19 +37,29 @@ class Catalogo : AppCompatActivity() {
         gridSeries.adapter = seriesAdapter
     }
     fun cargarPeliculas(){
-        peliculas.add(Pelicula("Demon Slayer: Kimetsu no Yaiba -To the Hashira Training", R.drawable.demo, R.drawable.demon,"Tanjiro y sus amigos entrenan con los Hashira para enfrentar nuevas amenazas demoníacas. La acción y la amistad se mezclan en esta aventura épica." ))
-        peliculas.add(Pelicula("Dune", R.drawable.dune, R.drawable.dune2,"En un futuro lejano, Paul Atreides lidera una rebelión en el desértico planeta Arrakis para controlar el recurso más valioso del universo, la especia." ))
-        peliculas.add(Pelicula( "Héroe Por Encargo", R.drawable.heroexencargo,R.drawable.heroeencargo, "Un hombre común se convierte en un héroe accidental al salvar a su ciudad de un desastre inminente, descubriendo su verdadero potencial en el proceso." ))
-        peliculas.add(Pelicula( "Madame Web",R.drawable.madameweb, R.drawable.madame,"Mientras tanto, en Nueva York, una mujer con habilidades psíquicas lucha contra el crimen, utilizando su conexión con una red mística." ))
-        peliculas.add(Pelicula( "Vidas Pasadas", R.drawable.vidaspasadas,R.drawable.vidaspasadas1, "Nora y Hae Sung, dos amigos de la infancia muy unidos, se separan después de que la familia de Nora emigra de Corea del Sur, pero el destino tiene otros planes para ellos." ))
+        peliculas.add(Pelicula("Demon Slayer: Kimetsu no Yaiba -To the Hashira Training", R.drawable.demo, R.drawable.demon,"Tanjiro y sus amigos entrenan con los Hashira para enfrentar nuevas amenazas demoníacas. La acción y la amistad se mezclan en esta aventura épica.",
+            arrayListOf<Cliente>() ))
+        peliculas.add(Pelicula("Dune", R.drawable.dune, R.drawable.dune2,"En un futuro lejano, Paul Atreides lidera una rebelión en el desértico planeta Arrakis para controlar el recurso más valioso del universo, la especia.",
+            arrayListOf<Cliente>() ))
+        peliculas.add(Pelicula( "Héroe Por Encargo", R.drawable.heroexencargo,R.drawable.heroeencargo, "Un hombre común se convierte en un héroe accidental al salvar a su ciudad de un desastre inminente, descubriendo su verdadero potencial en el proceso.",
+            arrayListOf<Cliente>() ))
+        peliculas.add(Pelicula( "Madame Web",R.drawable.madameweb, R.drawable.madame,"Mientras tanto, en Nueva York, una mujer con habilidades psíquicas lucha contra el crimen, utilizando su conexión con una red mística.",
+            arrayListOf<Cliente>() ))
+        peliculas.add(Pelicula( "Vidas Pasadas", R.drawable.vidaspasadas,R.drawable.vidaspasadas1, "Nora y Hae Sung, dos amigos de la infancia muy unidos, se separan después de que la familia de Nora emigra de Corea del Sur, pero el destino tiene otros planes para ellos.",
+            arrayListOf<Cliente>() ))
     }
 
     fun cargarSeries(){
-        series.add(Pelicula( "Halo",R.drawable.halo, R.drawable.halos, "Una evacuación mortal cambia la vida de la Dra. Halsey y la lleva a crear el programa de supersoldados Spartan para enfrentar a una amenaza alienígena." ))
-        series.add(Pelicula( "Leveling", R.drawable.sololeveling,R.drawable.sololevelings,"En un mundo donde los cazadores luchan contra monstruos, un cazador de bajo nivel descubre un poder oculto que podría cambiarlo todo." ))
-        series.add(Pelicula( "Mi adorable demonio", R.drawable.adorabledemonios,R.drawable.adorabledemonio,"Una joven descubre que su nuevo compañero de cuarto es un demonio adorable con un oscuro secreto y debe aprender a convivir con él." ))
-        series.add(Pelicula("El Monstruo de la Vieja Seúl",R.drawable.elmonstruo, R.drawable.elmonstruovieja,"En la antigua Seúl, un monstruo despierta y aterroriza a la ciudad. Solo un grupo de héroes puede detenerlo." ))
-        series.add(Pelicula( "Witcher", R.drawable.thewitcher, R.drawable.thewitchers,"Geralt de Rivia, un cazador de monstruos, lucha por encontrar su lugar en un mundo donde las personas suelen ser más perversas que las bestias." ))
+        series.add(Pelicula( "Halo",R.drawable.halo, R.drawable.halos, "Una evacuación mortal cambia la vida de la Dra. Halsey y la lleva a crear el programa de supersoldados Spartan para enfrentar a una amenaza alienígena.",
+            arrayListOf<Cliente>() ))
+        series.add(Pelicula( "Leveling", R.drawable.sololeveling,R.drawable.sololevelings,"En un mundo donde los cazadores luchan contra monstruos, un cazador de bajo nivel descubre un poder oculto que podría cambiarlo todo.",
+            arrayListOf<Cliente>() ))
+        series.add(Pelicula( "Mi adorable demonio", R.drawable.adorabledemonios,R.drawable.adorabledemonio,"Una joven descubre que su nuevo compañero de cuarto es un demonio adorable con un oscuro secreto y debe aprender a convivir con él.",
+            arrayListOf<Cliente>() ))
+        series.add(Pelicula("El Monstruo de la Vieja Seúl",R.drawable.elmonstruo, R.drawable.elmonstruovieja,"En la antigua Seúl, un monstruo despierta y aterroriza a la ciudad. Solo un grupo de héroes puede detenerlo.",
+            arrayListOf<Cliente>() ))
+        series.add(Pelicula( "Witcher", R.drawable.thewitcher, R.drawable.thewitchers,"Geralt de Rivia, un cazador de monstruos, lucha por encontrar su lugar en un mundo donde las personas suelen ser más perversas que las bestias.",
+            arrayListOf<Cliente>()  ))
     }
 
     }
@@ -82,6 +92,7 @@ class PeliculaAdapter(var context: Context?, var peliculas: ArrayList<Pelicula>)
             intento.putExtra("imagen", pelicula.image)
             intento.putExtra("header", pelicula.header)
             intento.putExtra("sinopsis", pelicula.sinopsis)
+            intento.putExtra("numberSeats",(20-pelicula.seats.size))
             context!!.startActivity(intento)
         }
 
