3fec00f24400b66578edeca942fd19cdd46510c4
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-03-06 23:17:16 -0700

    practica 6 terminada

--- Practica6/app/src/main/java/mx/edu/itson/practica6/Catalogo.kt
+++ Practica6/app/src/main/java/mx/edu/itson/practica6/Catalogo.kt
@@ -1,20 +1,92 @@
 package mx.edu.itson.practica6
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Catalogo : AppCompatActivity() {
+    var adapter: PeliculaAdapter? = null
+    var seriesAdapter: PeliculaAdapter? = null
+    var peliculas = ArrayList<Pelicula>()
+    var series = ArrayList<Pelicula>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_catalogo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        cargarPeliculas()
+        cargarSeries()
+
+        adapter = PeliculaAdapter( this, peliculas)
+        seriesAdapter = PeliculaAdapter(this, series)
+        var gridPelis: GridView = findViewById(R.id.movies_catalogo)
+        var gridSeries: GridView = findViewById(R.id.mseries_catalogo)
+
+        gridPelis.adapter = adapter
+        gridSeries.adapter = seriesAdapter
     }
+    fun cargarPeliculas(){
+        peliculas.add(Pelicula("Demon Slayer: Kimetsu no Yaiba -To the Hashira Training", R.drawable.demo, R.drawable.demon,"Tanjiro y sus amigos entrenan con los Hashira para enfrentar nuevas amenazas demoníacas. La acción y la amistad se mezclan en esta aventura épica." ))
+        peliculas.add(Pelicula("Dune", R.drawable.dune, R.drawable.dune2,"En un futuro lejano, Paul Atreides lidera una rebelión en el desértico planeta Arrakis para controlar el recurso más valioso del universo, la especia." ))
+        peliculas.add(Pelicula( "Héroe Por Encargo", R.drawable.heroexencargo,R.drawable.heroeencargo, "Un hombre común se convierte en un héroe accidental al salvar a su ciudad de un desastre inminente, descubriendo su verdadero potencial en el proceso." ))
+        peliculas.add(Pelicula( "Madame Web",R.drawable.madameweb, R.drawable.madame,"Mientras tanto, en Nueva York, una mujer con habilidades psíquicas lucha contra el crimen, utilizando su conexión con una red mística." ))
+        peliculas.add(Pelicula( "Vidas Pasadas", R.drawable.vidaspasadas,R.drawable.vidaspasadas1, "Nora y Hae Sung, dos amigos de la infancia muy unidos, se separan después de que la familia de Nora emigra de Corea del Sur, pero el destino tiene otros planes para ellos." ))
+    }
+
+    fun cargarSeries(){
+        series.add(Pelicula( "Halo",R.drawable.halo, R.drawable.halos, "Una evacuación mortal cambia la vida de la Dra. Halsey y la lleva a crear el programa de supersoldados Spartan para enfrentar a una amenaza alienígena." ))
+        series.add(Pelicula( "Leveling", R.drawable.sololeveling,R.drawable.sololevelings,"En un mundo donde los cazadores luchan contra monstruos, un cazador de bajo nivel descubre un poder oculto que podría cambiarlo todo." ))
+        series.add(Pelicula( "Mi adorable demonio", R.drawable.adorabledemonios,R.drawable.adorabledemonio,"Una joven descubre que su nuevo compañero de cuarto es un demonio adorable con un oscuro secreto y debe aprender a convivir con él." ))
+        series.add(Pelicula("El Monstruo de la Vieja Seúl",R.drawable.elmonstruo, R.drawable.elmonstruovieja,"En la antigua Seúl, un monstruo despierta y aterroriza a la ciudad. Solo un grupo de héroes puede detenerlo." ))
+        series.add(Pelicula( "Witcher", R.drawable.thewitcher, R.drawable.thewitchers,"Geralt de Rivia, un cazador de monstruos, lucha por encontrar su lugar en un mundo donde las personas suelen ser más perversas que las bestias." ))
+    }
+
+    }
+class PeliculaAdapter(var context: Context?, var peliculas: ArrayList<Pelicula>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return peliculas.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return peliculas[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(pe: Int, p1: View?, p2: ViewGroup?): View {
+        var pelicula = peliculas[pe]
+        var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var vista = inflator.inflate(R.layout.pelicula, null)
+        var image: ImageView = vista.findViewById(R.id.image_movie_cell)
+        var title: TextView = vista.findViewById(R.id.movie_title_cell)
+
+        image.setImageResource(pelicula.image)
+        title.setText(pelicula.titulo)
+
+        image.setOnClickListener() {
+            val intento = Intent(context, detalle_pelicula::class.java)
+            intento.putExtra("titulo", pelicula.titulo)
+            intento.putExtra("imagen", pelicula.image)
+            intento.putExtra("header", pelicula.header)
+            intento.putExtra("sinopsis", pelicula.sinopsis)
+            context!!.startActivity(intento)
+        }
+
+        return vista
+    }
+
+
 }
--- Practica6/app/src/main/java/mx/edu/itson/practica6/MainActivity.kt
+++ Practica6/app/src/main/java/mx/edu/itson/practica6/MainActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.practica6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,14 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button : Button = findViewById(R.id.boton)
+
+        button.setOnClickListener(){
+            var intent: Intent = Intent(this, Catalogo::class.java)
+            startActivity(intent)
         }
+
     }
 }
