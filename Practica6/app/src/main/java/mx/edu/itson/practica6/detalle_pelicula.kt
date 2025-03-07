b4d846afa5f0c4b3a0e112e8e1c7421988a10c97
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-03-07 00:12:36 -0700

    se agregó el botón

--- Practica6/app/src/main/java/mx/edu/itson/practica6/detalle_pelicula.kt
+++ Practica6/app/src/main/java/mx/edu/itson/practica6/detalle_pelicula.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.practica6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +18,13 @@ class detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val btnBuyTicket: Button = findViewById(R.id.buyTickets)
+
+        btnBuyTicket.setOnClickListener {
+            var intent : Intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
+        }
+
 
         val bundle = intent.extras
         if (bundle != null) {
